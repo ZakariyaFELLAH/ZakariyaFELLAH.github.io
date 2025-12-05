98a973e966dd4dcb8f50126b36f4b56b9bb4f46f
Zakariya FELLAH <zakariya.fellah92@gmail.com>
2025-12-05 16:07:21 +0100

    Add files via upload

---
+++ img/Veille.mm
@@ -0,0 +1,194 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<bookmarks>
+    <bookmark nodeId="ID_1090958577" name="Racine" opensAsRoot="true"/>
+</bookmarks>
+<node TEXT="CYBER-SECURITE" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1760273269785" VGAP_QUANTITY="3 pt">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#002b36ff" zoom="0.2896643">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" auto_compact_layout="true" show_tags="UNDER_NODES" show_note_icons="true" associatedTemplateLocation="template:/dark_solarized_template.mm" fit_to_viewport="false" show_icons="BESIDE_NODES" showTagCategories="false"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1664261437" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#002b36" BACKGROUND_COLOR="#fdf6e3" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#fdf6e3" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1664261437" STARTINCLINATION="132.6 pt;-30 pt;" ENDINCLINATION="132.6 pt;24 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#93a1a1" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#fdf6e3" BACKGROUND_COLOR="#073642">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#2aa198" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10" ITALIC="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#fdf6e3" BACKGROUND_COLOR="#d33682" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#d33682"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_1940777954" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_1940777954"/>
+<font NAME="Ubuntu" SIZE="12" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#002b36" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_WIDTH="3.1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#fdf6e3" BACKGROUND_COLOR="#073642" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#fdf6e3" BACKGROUND_COLOR="#586e75" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#fdf6e3" BACKGROUND_COLOR="#657b83" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#fdf6e3" BACKGROUND_COLOR="#839496" BORDER_COLOR_LIKE_EDGE="true">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#fdf6e3" BACKGROUND_COLOR="#93a1a1" BORDER_COLOR_LIKE_EDGE="true">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#eee8d5" BORDER_COLOR_LIKE_EDGE="true">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#eedfcc" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#fdf6e3" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Définition" POSITION="bottom_or_right" ID="ID_1266288810" CREATED="1759314321095" MODIFIED="1760268342018" HGAP_QUANTITY="238.24999 pt" VSHIFT_QUANTITY="15 pt" VGAP_QUANTITY="3 pt">
+<font ITALIC="true"/>
+<node TEXT="La cybersécurité désigne l&apos;ensemble des pratiques, technologies et processus conçus pour protéger les systèmes informatiques et les réseaux contre les attaques numériques. Elle vise à garantir la confidentialité, l&apos;intégrité et la disponibilité des informations numériques, en protégeant les données sensibles des accès non autorisés et des dommages." ID="ID_1195133747" CREATED="1759314647063" MODIFIED="1759315123112" HGAP_QUANTITY="35.75 pt" VSHIFT_QUANTITY="-77.25 pt">
+<font ITALIC="true"/>
+</node>
+</node>
+<node TEXT="Lieux" POSITION="top_or_left" ID="ID_1992643864" CREATED="1759314354210" MODIFIED="1760269002966" HGAP_QUANTITY="98 pt" VSHIFT_QUANTITY="-83.25 pt">
+<font ITALIC="true"/>
+<node TEXT="Cyber-Show 2025 - 28 et 29 Janvier" ID="ID_553168840" CREATED="1759314972427" MODIFIED="1759315119186">
+<font ITALIC="true"/>
+</node>
+<node TEXT="Forum Cybersec Cloud IA 2025 - 7 et 8 octobre Paris" ID="ID_1958494992" CREATED="1759315023682" MODIFIED="1759315083610">
+<font ITALIC="true"/>
+</node>
+</node>
+<node TEXT="Termes Principaux" POSITION="bottom_or_right" ID="ID_598305848" CREATED="1759314378132" MODIFIED="1760269000459" HGAP_QUANTITY="261.49999 pt" VSHIFT_QUANTITY="42.75 pt" VGAP_QUANTITY="3 pt">
+<font ITALIC="true"/>
+<node TEXT="Disponibilité" ID="ID_142990959" CREATED="1759314683231" MODIFIED="1759314940123">
+<font BOLD="true" ITALIC="false"/>
+</node>
+<node TEXT="Confidentialité" ID="ID_156849784" CREATED="1759314888387" MODIFIED="1759314941356">
+<font BOLD="true"/>
+</node>
+<node TEXT="Intégrité" ID="ID_20082162" CREATED="1759314912620" MODIFIED="1759314943473">
+<font BOLD="true"/>
+</node>
+<node TEXT="Preuve" ID="ID_1304773926" CREATED="1759314924577" MODIFIED="1759314945122">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="News" POSITION="top_or_left" ID="ID_125275004" CREATED="1759314379931" MODIFIED="1760270574306" HGAP_QUANTITY="301.99999 pt" VSHIFT_QUANTITY="-62.25 pt" VGAP_QUANTITY="3 pt">
+<font ITALIC="true"/>
+<node TEXT="CyberVeille: Septembre 2025 - Une IA créer et exécute son rançongiciel" ID="ID_538259730" CREATED="1760101173187" MODIFIED="1760268468542" HGAP_QUANTITY="53 pt" VSHIFT_QUANTITY="1.5 pt">
+<font BOLD="false" ITALIC="true"/>
+</node>
+<node TEXT="Sources:&#xa;&#xa;- ANSSI&#xa;&#xa;- CNIL&#xa;&#xa;- NIST Cybersecurity Framework&#xa;&#xa;- ISO 27001" ID="ID_1444445250" CREATED="1760268495381" MODIFIED="1760268702338" HGAP_QUANTITY="159.5 pt" VSHIFT_QUANTITY="114.75 pt">
+<font BOLD="true" UNDERLINED="true" ITALIC="false"/>
+</node>
+</node>
+<node TEXT="Outils et technologies" POSITION="bottom_or_right" ID="ID_408688257" CREATED="1760267377415" MODIFIED="1760268369828" HGAP_QUANTITY="-185.49999 pt" VSHIFT_QUANTITY="-277.49999 pt" VGAP_QUANTITY="3 pt">
+<font ITALIC="true"/>
+<node TEXT="- Wireshark&#xa;&#xa;- Metasploit&#xa;&#xa;- Kali Linux&#xa;&#xa;- Firewall&#xa;&#xa;- Chiffrement RSA / AES" ID="ID_1301907821" CREATED="1760268327694" MODIFIED="1760268374255" HGAP_QUANTITY="-176.49999 pt" VSHIFT_QUANTITY="-247.49999 pt">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="Risques et menaces" POSITION="top_or_left" ID="ID_179461300" CREATED="1760267384107" MODIFIED="1760271025000" HGAP_QUANTITY="-503.49998 pt" VSHIFT_QUANTITY="262.49999 pt" VGAP_QUANTITY="3 pt">
+<font ITALIC="true"/>
+<node TEXT="- Ransomware&#xa;&#xa;- Phishing&#xa;&#xa;- DDoS&#xa;&#xa;- Malware / virus&#xa;&#xa;- Ingénierie sociale" ID="ID_1767960630" CREATED="1760267462067" MODIFIED="1760271028217" HGAP_QUANTITY="-399.24999 pt" VSHIFT_QUANTITY="165.75 pt" VGAP_QUANTITY="3 pt">
+<font BOLD="true"/>
+<node TEXT="Types d&apos;attaquants" ID="ID_435059543" CREATED="1760271026018" MODIFIED="1760271047857" HGAP_QUANTITY="-361.74999 pt" VSHIFT_QUANTITY="-12.75 pt" VGAP_QUANTITY="3 pt">
+<font ITALIC="true"/>
+<node TEXT="- Script Kiddie&#xa;&#xa;- Cybercriminel&#xa;&#xa;- Hacktiviste&#xa;&#xa;- Insider&#xa;&#xa;- Advanced Persistent Threat (APT)&#xa;&#xa;- State-sponsored&#xa;&#xa;- Scripted/Automated Bots&#xa;&#xa;- Opportunistic attacker" ID="ID_461241095" CREATED="1760271043836" MODIFIED="1760271187222" HGAP_QUANTITY="-399.99999 pt">
+<font BOLD="true"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Mesures de protection" POSITION="bottom_or_right" ID="ID_442987951" CREATED="1760267408612" MODIFIED="1760267688678" HGAP_QUANTITY="158.75 pt" VSHIFT_QUANTITY="-30.75 pt">
+<font ITALIC="true"/>
+<node TEXT="- Mots de passe et MFA&#xa;&#xa;- Sauvegardes&#xa;&#xa;- Mises à jour&#xa;&#xa;- Sensibilisation utilisateurs&#xa;&#xa;- Antivirus / pare-feu / chiffrement" ID="ID_1663062536" CREATED="1760267679396" MODIFIED="1760267712469">
+<font BOLD="true" ITALIC="false"/>
+</node>
+</node>
+<node TEXT="Cadre Juridique/Acteurs" POSITION="top_or_left" ID="ID_540943930" CREATED="1760268745291" MODIFIED="1764946613586" HGAP_QUANTITY="-28.75 pt" VSHIFT_QUANTITY="-384.74999 pt" VGAP_QUANTITY="3 pt">
+<font BOLD="true"/>
+<node TEXT="FRANCE" ID="ID_1687791245" CREATED="1760268769945" MODIFIED="1760269473663" HGAP_QUANTITY="80.75 pt" VSHIFT_QUANTITY="150 pt" VGAP_QUANTITY="3 pt">
+<font BOLD="true" ITALIC="true"/>
+<node TEXT="- CNIL&#xa;&#xa;- ANSSI&#xa;&#xa;- ARCOM&#xa;&#xa;- Ministère de l’Intérieur&#xa;&#xa;- Gendarmerie Nationale (C3N, OCLCTIC)&#xa;&#xa;- Tribunal judiciaire (pôle cybercriminalité de Paris)" ID="ID_311113324" CREATED="1760269426507" MODIFIED="1760269559830" HGAP_QUANTITY="77.75 pt" VSHIFT_QUANTITY="152.25 pt">
+<font BOLD="true" ITALIC="false"/>
+</node>
+</node>
+<node TEXT="EUROPE" ID="ID_102643677" CREATED="1760268954992" MODIFIED="1760269542522" HGAP_QUANTITY="-109 pt" VSHIFT_QUANTITY="8.25 pt" VGAP_QUANTITY="3 pt">
+<font BOLD="true" ITALIC="true"/>
+<node TEXT="- Commission Européenne&#xa;&#xa;- ENISA – Agence Européenne pour la Cybersécurité&#xa;&#xa;- EDPB – Comité Européen de la Protection des Données&#xa;&#xa;- CJUE – Cour de Justice de l’Union Européenne&#xa;&#xa;- Conseil de l’Union Européenne&#xa;&#xa;- Europol (EC3) – Centre Européen de lutte contre la cybercriminalité" ID="ID_476376713" CREATED="1760269478674" MODIFIED="1760269542521" HGAP_QUANTITY="-109.75 pt" VSHIFT_QUANTITY="240.74999 pt">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="INTERNATIONAL" ID="ID_1383018317" CREATED="1760268966996" MODIFIED="1760269645651" HGAP_QUANTITY="-440.49999 pt" VSHIFT_QUANTITY="-231.74999 pt" VGAP_QUANTITY="3 pt">
+<font BOLD="true" ITALIC="true"/>
+<node TEXT="- ISO – Organisation Internationale de Normalisation&#xa;&#xa;- NIST – National Institute of Standards and Technology (États-Unis)&#xa;&#xa;- OTAN – Organisation du Traité de l’Atlantique Nord (Cyber Defence Centre of Excellence)&#xa;&#xa;- ITU – Union Internationale des Télécommunications&#xa;&#xa;_ UNODC – Office des Nations Unies contre la Drogue et le Crime" ID="ID_1485893030" CREATED="1760269479591" MODIFIED="1760269649184" HGAP_QUANTITY="-242.49999 pt" VSHIFT_QUANTITY="227.24999 pt">
+<font BOLD="true"/>
+</node>
+</node>
+</node>
+<node TEXT="Métiers" POSITION="bottom_or_right" ID="ID_85802991" CREATED="1760270575090" MODIFIED="1760270594389" HGAP_QUANTITY="-513.24998 pt" VSHIFT_QUANTITY="-174.74999 pt" VGAP_QUANTITY="3 pt">
+<font BOLD="false" ITALIC="true"/>
+<node TEXT="- Pentesteur&#xa;&#xa;- RSSI&#xa;&#xa;- Technicien en cybersécurité&#xa;&#xa;- Analyste SOC&#xa;&#xa;- Consultant en cybersécurité" ID="ID_1847139707" CREATED="1760270586873" MODIFIED="1760273239499" HGAP_QUANTITY="-429.24999 pt" VSHIFT_QUANTITY="105.75 pt">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="Formation" POSITION="top_or_left" ID="ID_1167506723" CREATED="1760270660490" MODIFIED="1760270718318" HGAP_QUANTITY="-672.24998 pt" VSHIFT_QUANTITY="-11.25 pt" VGAP_QUANTITY="3 pt">
+<node TEXT="Généraliste" ID="ID_212157111" CREATED="1760270704023" MODIFIED="1760270831495" HGAP_QUANTITY="-226.74999 pt" VSHIFT_QUANTITY="0.75 pt" VGAP_QUANTITY="3 pt">
+<font ITALIC="true"/>
+<node TEXT="- Prépa&#xa;- Licence&#xa;- Master&#xa;- Doctorat" ID="ID_1267998063" CREATED="1760270757391" MODIFIED="1760270856560" HGAP_QUANTITY="-225.24999 pt" VSHIFT_QUANTITY="-79.5 pt">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="Spécialiste" ID="ID_1208209646" CREATED="1760270743958" MODIFIED="1760270854301" HGAP_QUANTITY="-224.49999 pt" VSHIFT_QUANTITY="42.75 pt" VGAP_QUANTITY="3 pt">
+<font ITALIC="true"/>
+<node TEXT="- BTS&#xa;- BUT&#xa;- Licence Pro&#xa;- Master Pro&#xa;- Ecole d&apos;ingénieur" ID="ID_124889106" CREATED="1760270816563" MODIFIED="1760270916400" HGAP_QUANTITY="-222.99999 pt" VSHIFT_QUANTITY="64.5 pt">
+<font BOLD="true"/>
+</node>
+</node>
+</node>
+</node>
+</map>
